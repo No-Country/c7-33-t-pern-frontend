c5cd060e9479f2b354f13faf4acb20635ba43919
Javier A <javiersettex@gmail.com>
2022-09-26 18:35:21 -0500

    appbar  create whit mui

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -4,15 +4,19 @@ import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Profile from '../pages/Profile'
 import Register from '../pages/Register'
+import Navbar from '../components/ui/Navbar'
+import Footer from '../components/ui/Footer'
 
 const Routers = () => (
   <BrowserRouter>
+    <Navbar />
     <Routes>
       <Route element={<Home />} path="/" />
       <Route element={<Login />} path="/login" />
       <Route element={<Profile />} path="/profile/:id" />
       <Route element={<Register />} path="/register" />
     </Routes>
+    <Footer />
   </BrowserRouter>
 )
 
