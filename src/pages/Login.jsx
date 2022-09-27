8d2b1c530bc409693eae5eab918eab361f2ef41e
Javier A <javiersettex@gmail.com>
2022-09-27 15:29:31 -0500

    form login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,60 @@
-import {Grid, Typography} from '@mui/material'
+import {Button, TextField, Typography, Grid, Box} from '@mui/material'
+import {useForm} from 'react-hook-form'
 
-const Login = () => (
-  <Grid
-    container
-    alignItems="center"
-    justifyContent="center"
-    sx={{height: '500px', backgroundColor: 'red'}}
-  >
-    <Grid item>
-      <Typography variant="overline">estoy en login</Typography>
-    </Grid>
-  </Grid>
-)
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
+  return (
+    <Box mt={{xs: 15, sm: 30}} mx="auto" p={2} sx={{maxWidth: 400}}>
+      <form style={{width: '100%'}} onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Email"
+              type="email"
+              variant="standard"
+              {...register('email', {required: true})}
+            />
+            {errors.email && (
+              <Typography color="error" variant="caption">
+                email invalid
+              </Typography>
+            )}
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Password"
+              type="password"
+              variant="standard"
+              {...register('password', {required: true})}
+            />
+            {errors.password && <span>This field is required</span>}
+          </Grid>
+        </Grid>
+        <Grid container mt={1} spacing={1}>
+          <Grid item xs={6}>
+            <Button fullWidth type="submit" variant="contained">
+              Login
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button fullWidth color="error" type="reset" variant="text">
+              Reset
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Box>
+  )
+}
 
 export default Login
